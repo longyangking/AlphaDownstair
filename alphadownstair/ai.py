dd22786432fcf428dd7a31eddbae60367351ae8d
Yang Long <longyang_123@yeah.net>
2018-09-28 10:35:35 +0800

    basic structure for AI

--- alphadownstair/ai.py
+++ alphadownstair/ai.py
@@ -37,39 +37,160 @@ class NeuralNetwork:
     def build_model(self):
         state_tensor = Input(shape=self.input_shape)
 
+        x = self.__conv_block(state_tensor, self.network_structure[0]['filters'], self.network_structure[0]['kernel_size'])
+
+        if len(self.network_structure) > 1:
+            for h in self.network_structure[1:]:
+                x = self.__res_block(x, h['filters'], h['kernel_size'])
+
+        action_values_tensor = self.__action_value_block(x)
+
+        model = Model(inputs=state_tensor, outputs=action_values_tensor)
+        model.compile(
+            loss='mse',
+            optimizer=Adam(self.learning_rate)
+        )
+
+        return model
 
     def __conv_block(self, x, filters, kernel_size=3):
+        '''
+        Convolutional layer block
+        '''
+        out = Conv2D(
+            filters = filters,
+            kernel_size = kernel_size,
+            padding = 'same',
+            activation='linear',
+            kernel_regularizer = regularizers.l2(self.l2_const)
+        )(x)
+        out = BatchNormalization(axis=1)(out)
+        out = LeakyReLU()(out)
+        return out
 
     def __res_block(self, x, filters, kernel_size=3):
+        '''
+        Residual layer block
+        '''
+        out = Conv2D(
+            filters = filters,
+            kernel_size = kernel_size,
+            padding = 'same',
+            activation='linear',
+            kernel_regularizer = regularizers.l2(self.l2_const)
+        )(x)
+        out = BatchNormalization(axis=1)(out)
+        out = Add()([out, x])
+        out = LeakyReLU()(out)
+        return out
 
     def __action_value_block(self, x):
+        '''
+        Action Values layer block
+        '''
+        out = Conv2D(
+            filters = 64,
+            kernel_size = (3,3),
+            padding = 'same',
+            activation='linear',
+            kernel_regularizer = regularizers.l2(self.l2_const)
+        )(x)
+        out = BatchNormalization(axis=1)(out)
+        out = LeakyReLU()(out)
+
+        out = Flatten()(out)
+        out = Dense(
+            64,
+            use_bias=False,
+            activation='linear',
+            kernel_regularizer= regularizers.l2(self.l2_const)
+		)(out)
+        out = LeakyReLU()(out)
+
+        action_values = Dense(
+			self.output_dim, 
+            use_bias=False,
+            activation='relu',
+            kernel_regularizer=regularizers.l2(self.l2_const),
+            name = 'action'
+			)(out)
+
+        return action_values
 
     def predict(self, state):
+        state = np.array(state)
+        states = state.reshape(1, self.state_shape)
+        action_values = self.model.predict(states)
+        return action_values[0]
 
-    def fit(self, dataset, epochs, batch_size):
+    def fit(self, states, action_values, epochs, batch_size):
+        history = self.model.fit(states, action_values, epochs=epochs, batch_size=batch_size, verbose=self.verbose)
+        return history
 
-    def update(self, dataset):
+    def update(self, states, action_values):
+        '''
+        Update the coefficients of network one updation
+        '''
+        loss = self.model.train_on_batch(states, action_values)
+        return loss
 
     def load_model(self, filename):
+        self.model.load_weights(filename)
 
     def save_model(self, filename):
+        self.model.save_weights(filename)
 
     def plot_model(self, filename):
-
+        from keras.utils import plot_model
+        plot_model(self.model, show_shapes=True, to_file=filename)
 
 class AI:
     def __init__(self, state_shape, action_dim, verbose=False):
+        self.state_shape = state_shape
+        self.action_dim = action_dim
+        self.verbose = verbose
+
+        network_structure = list()
+        network_structure.append({'filters':64, 'kernel_size':3})
+        network_structure.append({'filters':64, 'kernel_size':3})
+        network_structure.append({'filters':64, 'kernel_size':3})
+        network_structure.append({'filters':64, 'kernel_size':3})
+
+        self.nnet =  NeuralNetwork(
+            input_shape=self.state_shape, 
+            output_dim=self.action_dim, 
+            network_structure=network_structure,
+            verbose=self.verbose
+        )
 
     def get_state_shape(self):
+        return np.copy(self.state_shape)
 
     def train(self, dataset, epochs, batch_size):
+        states, action_values = dataset
+        history = self.nnet.fit(states, action_values, epochs=epochs, batch_size=batch_size)
+        return history
 
     def update(self, dataset):
+        states, action_values = dataset
+        loss = self.nnet.update(states, action_values)
+        return loss
 
-    def play(self):
+    def evaluate_function(self, state):
+        action_value = self.nnet.predict(state)
+        action = np.argmax(action_value)
+        return action, action_value
+
+    def play(self, state):
+        action_value = self.nnet.predict(state)
+        action = np.argmax(action_value)
+        return action
 
     def load_nnet(self, filename):
+        self.nnet.load_model(filename)
 
     def save_nnet(self, filename):
+        self.nnet.save_model(filename)
 
     def plot_nnet(self, filename):
+        self.nnet.plot_model(filename)
--- alphadownstair/train.py
+++ alphadownstair/train.py
@@ -1,1 +1,139 @@
 import numpy as np 
+from ai import AI
+
+class SelfplayEngine:
+    def __init__(self, ai, verbose=False):
+        self.ai = ai 
+        self.state_shape = ai.get_state_shape()
+        self.verbose = verbose
+
+        # Train data
+        self.states = list()
+        self.boards = list()
+        self.actions = list()
+        self.scores = list()
+
+    def get_state(self):
+        return self.states[-1]
+
+    def update_states(self):
+        '''
+        Update stored states
+        '''
+        Nx, Ny, channel = self.state_shape
+        state = np.zeros((Nx,Ny,channel))
+        n_areas = len(self.areas)
+        for i in range(channel):
+            if i+1 <= n_areas:
+                state[:,:,-(i+1)] = self.areas[-(i+1)]
+
+        self.states.append(state)
+
+    def start(self):
+        '''
+        The main process of self-play training
+        '''
+
+        # TODO  It would be the key to define a appropriate action-value function, 
+        #       which would be one of the main parts in this code project.
+        
+        action_values = list()
+
+        return self.states, action_values
+
+
+class TrainAI:
+    def __init__(self, state_shape, action_dim=4, ai=None, verbose=False):
+        self.state_shape = state_shape
+        self.action_dim = action_dim
+        self.verbose = verbose
+
+        if ai is not None:
+            self.ai = ai 
+        else:
+            self.ai = AI(
+                state_shape=state_shape,
+                action_dim=action_dim,
+                verbose=verbose            
+            )
+
+        self.losses = list()
+    
+    def get_losses(self):
+        return np.array(self.losses)
+
+    def get_selfplay_data(self, n_rounds):
+        states = list()
+        action_values = list()
+
+        if self.verbose:
+            starttime = time.time()
+            print("Start self-play process with rounds [{0}]:".format(n_rounds))
+
+        for i in range(n_rounds):
+            if self.verbose:
+                print("{0}th self-play round...".format(i+1))
+
+            engine = SelfplayEngine(
+                ai=self.ai,
+                verbose=self.verbose
+            )
+
+            _states, _action_values = engine.start()
+            for i in range(len(_action_values)):
+                states.append(_states[i])
+                action_values.append(_action_values[i])
+        
+        if self.verbose:
+            endtime = time.time()
+            print("End of self-play process with data size [{0}] and cost time [{1:.1f}s].".format(
+                len(action_values),  (endtime - starttime)))
+
+        states = np.array(states)
+        action_values = np.array(action_values)
+
+        return states, action_values
+
+    def update_ai(self, dataset):
+        if self.verbose:
+            print("Start to update the network of AI model...")
+
+        history = self.ai.train(dataset, epochs=30, batch_size=32)
+
+        if self.verbose:
+            print("End of updating with final loss [{0:.4f}]".format(history.history['loss'][-1]))
+
+        return history
+
+    def start(self, filename):
+        '''
+        Main training process
+        '''
+        n_epochs = 1000
+        n_rounds = 30
+        n_checkpoints = 10
+
+        if self.verbose:
+            print("Train AI model with epochs: [{0}]".format(n_epochs))
+        
+        for i in range(n_epochs):
+            if self.verbose:
+                print("{0}th self-play training process ...".format(i+1))
+
+            dataset = self.get_selfplay_data(n_rounds)
+
+            history = self.update_ai(dataset)
+            self.losses.extend(history.history['loss'])
+
+            if self.verbose:
+                print("End of training process.")
+
+            if (i+1)%n_checkpoints == 0:
+                if self.verbose:
+                    print("Checkpoint: Saving AI model with filename [{0}] ...".format(filename),end="")
+
+                self.ai.save_nnet(filename)
+
+                if self.verbose:
+                    print("OK!")
+        
